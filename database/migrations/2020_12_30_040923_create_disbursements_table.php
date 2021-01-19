e06d507d5e544aa08be5ad946cd22db13ffc3eda
salman <noorislamsalman@gmail.com>
2021-01-19 13:04:45 +0600

    disbursement migration is_disbursement default value added

--- database/migrations/2020_12_30_040923_create_disbursements_table.php
+++ database/migrations/2020_12_30_040923_create_disbursements_table.php
@@ -18,7 +18,7 @@ class CreateDisbursementsTable extends Migration
             $table->string('disburse_id');
             $table->string('commission_amount');
             $table->string('store_id');
-            $table->string('is_disbursement');
+            $table->string('is_disbursement')->default('0');
             $table->string('payment_amount');
             $table->string('payment_detail');
             $table->string('net_payable');
